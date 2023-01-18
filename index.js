be52b6fd46ca6d8e248bc97f21096d7877d78fc4
tcsmismailalam <111044117+tcsmismailalam@users.noreply.github.com>
2023-01-18 13:13:22 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,132 @@
-// Import packages
 const express = require("express");
+const cors = require("cors");
 
-// Middlewares
 const app = express();
+
+app.use(cors());
+
+// parse requests of content-type - application/json
 app.use(express.json());
 
-// Routes
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+const Role = db.role;
+const LeaveType = db.leaveType;
+// const uri = `mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`;
+const uri =
+  "mongodb+srv://smismailalam:adminLS786@ismailalam.0je8p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+db.mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    // initial();
+  })
+  .catch((err) => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+// simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Test application." });
 });
+// routes
+// require("./app/routes/auth.routes")(app);
+// require("./app/routes/user.routes")(app);
+// require("./app/routes/leaveType.routes")(app);
 
-// connection
-const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Listening to port ${port}`));
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "moderator",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'moderator' to roles collection");
+      });
+
+      new Role({
+        name: "admin",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+
+  LeaveType.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new LeaveType({
+        name: "Annual Leave",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+        console.log("added 'user' to roles collection");
+      });
+
+      new LeaveType({
+        name: "Bereavement Leave",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+        console.log("added 'user' to roles collection");
+      });
+
+      new LeaveType({
+        name: "Birthday Leave",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+        console.log("added 'user' to roles collection");
+      });
+
+      new LeaveType({
+        name: "Casual Leave",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+        console.log("added 'user' to roles collection");
+      });
+
+      new LeaveType({
+        name: "Sick Leave",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+        console.log("added 'user' to roles collection");
+      });
+    }
+  });
+}
